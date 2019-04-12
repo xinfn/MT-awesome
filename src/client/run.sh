cb296f3a9d24b25d3064bffaa831a71a7850ce7a
bingzhen.pan <panbingzhen@qq.com>
2019-04-12 15:47:57 +0800

    Update client

--- src/client/run.sh
+++ src/client/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-curl "http://127.0.0.1:8080/ping"
+# create account 'account1'
+curl -v -X POST "http://127.0.0.1:8080/account?name=account1&password=123456"
+
+# get account 'account1'
+curl -v "http://127.0.0.1:8080/account?name=account1"
