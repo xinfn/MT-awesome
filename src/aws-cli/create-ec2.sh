9adc7f06fb3eb64e1bf90905271835ff6a6efe34
panbingzhen <panbingzhen@qq.com>
2019-04-15 03:29:06 +0000

    Auto terminate EC2 after 1 hour

--- src/aws-cli/create-ec2.sh
+++ src/aws-cli/create-ec2.sh
@@ -6,4 +6,4 @@ IMAGEID="ami-0dd1edad280d16cf3"
 USERDATA=file://$BASEDIR/user-data.txt
 
 # create EC2 instance and save instance id to aws-instance-ids.txt
-aws --region cn-northwest-1 ec2 run-instances --instance-type t2.micro --image-id $IMAGEID --key-name devops_node_key_pair --security-group-ids sg-0e9bb1e4d3acbe470 --subnet-id subnet-b6af66cd --associate-public-ip-address --query "Instances[0].InstanceId" --user-data "${USERDATA}" > $BASEDIR/aws-instance-ids.txt && echo "Instances starting" && cat $BASEDIR/aws-instance-ids.txt | xargs aws --region cn-northwest-1 ec2 wait instance-running --instance-ids && echo "Instances started"
+aws --region cn-northwest-1 ec2 run-instances --instance-type t2.micro --image-id $IMAGEID --key-name devops_node_key_pair --security-group-ids sg-0e9bb1e4d3acbe470 --subnet-id subnet-b6af66cd --associate-public-ip-address --instance-initiated-shutdown-behavior terminate --query "Instances[0].InstanceId" --user-data "${USERDATA}" > $BASEDIR/aws-instance-ids.txt && echo "Instances starting" && cat $BASEDIR/aws-instance-ids.txt | xargs aws --region cn-northwest-1 ec2 wait instance-running --instance-ids && echo "Instances started"
