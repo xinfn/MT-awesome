f683cf07e3822f5708f9f3a7002212cca1e39f18
panbingzhen <panbingzhen@qq.com>
2019-04-15 15:43:42 +0800

    Add path routing for CICD

--- src/server/main.go
+++ src/server/main.go
@@ -3,12 +3,19 @@ package main
 import "server/api"
 import "github.com/gin-gonic/gin"
 
+func initRoute(group *gin.RouterGroup) {
+	group.POST("/account", api.CreateAccount)
+	group.GET("/account", api.GetAccount)
+}
+
 func main() {
 	r := gin.Default()
 
-	// Register endpoint and handler
-	r.POST("/account", api.CreateAccount)
-	r.GET("/account", api.GetAccount)
+	// Normal routing
+	initRoute(r.Group("/"))
+
+	// Route for CICD path start with /bxx/
+	initRoute(r.Group("/b:id"))
 
 	// run HTTP server
 	r.Run("0.0.0.0:8080")
