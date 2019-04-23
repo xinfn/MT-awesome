7b31eb8ae6dd61ed4ee1b99d841c311d24952e93
zhengfuyuan <846820279@qq.com>
2019-04-23 09:59:17 +0000

    Update Jenkinsfile.

--- terraform/init_and_apply.sh
+++ terraform/init_and_apply.sh
@@ -5,8 +5,6 @@ BUILD_ID=$1
 
 echo "ENV=${ENV} BUILD_ID=${BUILD_ID}" >> log.txt
 
-exit
-
 terraform init -backend-config="key=infrastructure/$ENV/mt-cicd.tfstate" -plugin-dir=/home/ubuntu/.terraform.d/plugin-cache/linux_amd64/ >> log.txt 2>&1
 
 terraform apply -auto-approve -var 'api_gataway_stage_name=${BUILD_ID}' >> log.txt 2>&1
